8ec00668380f0614619fcb0ad3d5eef94253e1b3
manueltobon <manuelpope@gmail.com>
2022-01-11 12:26:28 -0300

    init

--- main.go
+++ main.go
@@ -2,54 +2,10 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"log"
 	"net/http"
-	"sync"
 )
-
-type Memory struct {
-	cache map[string]string
-	lock  sync.Mutex
-}
-
-func (m *Memory) Get(key string) (string, error) {
-	m.lock.Lock()
-	result, exists := m.cache[key]
-	m.lock.Unlock()
-	if !exists {
-		log.Println("Does not exist that key ", key)
-		return "", errors.New("Not contained that key")
-	}
-	return result, nil
-}
-
-func (m *Memory) Set(key string, value string) (string, error) {
-	m.lock.Lock()
-	_, exists := m.cache[key]
-	m.lock.Unlock()
-	if !exists {
-		m.lock.Lock()
-		m.cache[key] = value
-		m.lock.Unlock()
-
-	}
-	return m.cache[key], nil
-}
-func (m *Memory) All() (map[string]string, error) {
-	m.lock.Lock()
-	result := cache.cache
-	m.lock.Unlock()
-	return result, nil
-
-}
-
-func NewCache() *Memory {
-	return &Memory{
-		cache: make(map[string]string),
-	}
-}
 
 var cache = NewCache()
 
@@ -57,10 +13,20 @@ func returnAllKeysValues(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	mapAux := map[string]string{}
-	mapAux, _ = cache.All()
+	key := r.URL.Query().Get("key")
+
+	if key == "" {
+		log.Println("Url Param 'key' is missing")
+		mapAux, _ = cache.All()
+		jsonString, _ := json.Marshal(mapAux)
+		w.Write(jsonString)
+		return
+	}
+
+	mapAux[key], _ = cache.Get(key)
+
 	jsonString, _ := json.Marshal(mapAux)
 	w.Write(jsonString)
-	log.Println(r, w)
 
 }
 
